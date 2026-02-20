54cb55cb7879070803d7b8dfa416453bdac1975a
ImrenR <imrenrahbay@gmail.com>
2026-02-20 13:27:43 +0000

    tailwind css v3 dowloanded

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // if you dont have this you cant see the classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
